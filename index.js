735d1edea234aa5c12168c8ea0cd84e71f07ccf5
Tahir J <icodejs@gmail.com>
2013-12-09 18:44:31 +0000

    Decoded base64 params on convert-image endpoint

--- index.js
+++ index.js
@@ -15,7 +15,8 @@ app.configure(function() {
 app.post('/convert-svg', convert.toSVG); // returns array of svg strings
 app.post('/convert-html', convert.toHTML); // returns n base64 images as HTML
 app.post('/convert-base64', convert.toBase64); // returns array of base64 strings
-app.post('/convert-image', convert.toImage); // returns image
+
+app.get('/convert-image/:base64', convert.toImage); // returns image
 app.get('/healthcheck', healthCheck.status);
 
 app.listen(Config.service.port);
